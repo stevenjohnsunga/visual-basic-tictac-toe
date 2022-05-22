862817bcf5da15b443e73272441064617b87511c
steven <steven17@gmail.com>
2022-05-23 02:02:29 +0800

    Added code to btn1 button

--- tic tac tie on visual basic/Form1.vb
+++ tic tac tie on visual basic/Form1.vb
@@ -1,5 +1,23 @@
 ﻿Public Class Form1
+    Dim count As Integer = 0
+    Dim isx As String = ""
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If count = 0 Then
+            btn1.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn1.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn1.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+
+
 
     End Sub
 
