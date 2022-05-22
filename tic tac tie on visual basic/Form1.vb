b5d69cb137d4fe22226bb37407a8385b44011f6d
steven <steven17@gmail.com>
2022-05-23 02:10:25 +0800

    added code to btn1 to btn 9

--- tic tac tie on visual basic/Form1.vb
+++ tic tac tie on visual basic/Form1.vb
@@ -16,7 +16,7 @@
             isx = "X"
             count = count + 1
         End If
-
+        btn1.Enabled = False
 
 
     End Sub
@@ -31,6 +31,164 @@
         btn7.Text = ""
         btn8.Text = ""
         btn9.Text = ""
+        count = 0
+    End Sub
 
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If count = 0 Then
+            btn2.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn2.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn2.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn2.Enabled = False
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If count = 0 Then
+            btn3.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn3.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn3.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn3.Enabled = False
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If count = 0 Then
+            btn4.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn4.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn4.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn4.Enabled = False
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If count = 0 Then
+            btn5.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn5.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn5.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn5.Enabled = False
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If count = 0 Then
+            btn6.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn6.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn6.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn6.Enabled = False
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If count = 0 Then
+            btn7.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn7.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn7.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn7.Enabled = False
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If count = 0 Then
+            btn8.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn8.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn8.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn8.Enabled = False
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If count = 0 Then
+            btn9.Text = "X"
+            isx = "X"
+            count = count + 1
+        ElseIf isx = "X" Then
+            btn9.Text = "O"
+            isx = "O"
+            count = count + 1
+        ElseIf isx = "O" Then
+            btn9.Text = "X"
+            isx = "X"
+            count = count + 1
+        End If
+        btn9.Enabled = False
+    End Sub
+
+    Private Sub btnreset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        btn1.Text = ""
+        btn2.Text = ""
+        btn3.Text = ""
+        btn4.Text = ""
+        btn5.Text = ""
+        btn6.Text = ""
+        btn7.Text = ""
+        btn8.Text = ""
+        btn9.Text = ""
+        count = 0
+        btn1.Enabled = True
+        btn2.Enabled = True
+        btn3.Enabled = True
+        btn4.Enabled = True
+        btn5.Enabled = True
+        btn6.Enabled = True
+        btn7.Enabled = True
+        btn8.Enabled = True
+        btn9.Enabled = True
     End Sub
 End Class
